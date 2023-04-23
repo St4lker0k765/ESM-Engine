47596151a7570ad10f3ca5e8fc0987f80a1aff0d
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2023-04-23 23:27:57 +0300

    Фикс крашей при запуске на xrRender_R1

--- trunk/xrCPU_Pipe/StdAfx.h
+++ trunk/xrCPU_Pipe/StdAfx.h
@@ -2,11 +2,17 @@
 
 #include "..\xr_3da\stdafx.h"
 
+#include "../xrCore/xrCore.h"
 #include "../xrRender/xrRender/SkeletonXVertRender.h"
 #include "../xr_3da/bone.h"
+#define RENDER 1
+#include "../xr_3da/Render.h"
+#include "../xr_3da/Device.h"
+#include "../xrRender/xrRender/light.h"
 
 #pragma warning(disable:4995)
 #include <xmmintrin.h>
 #pragma warning(default:4995)
 
+#include "xrCPU_Pipe.h"
 #include "ttapi.h"

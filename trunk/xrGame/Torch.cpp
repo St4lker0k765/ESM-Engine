57940a1f1274cfe92ed7ba4cd7f151180208e2eb
morrazzzz <123486080+morrazzzz@users.noreply.github.com>
2023-05-03 22:12:49 +0300

    Шизофрения

--- trunk/xrGame/Torch.cpp
+++ trunk/xrGame/Torch.cpp
@@ -228,9 +228,7 @@ BOOL CTorch::net_Spawn(CSE_Abstract* DC)
 	if (!inherited::net_Spawn(DC))
 		return				(FALSE);
 	
-	bool b_r2				= !!psDeviceFlags.test(rsR2);
-	b_r2                    |= !!psDeviceFlags.test(rsR3);
-	b_r2                    |= !!psDeviceFlags.test(rsR4);
+	bool b_r2				= !!psDeviceFlags.test(rsR2) || !!psDeviceFlags.test(rsR3) || !!psDeviceFlags.test(rsR4);
 
 	IKinematics* K			= smart_cast<IKinematics*>(Visual());
 	CInifile* pUserData		= K->LL_UserData(); 

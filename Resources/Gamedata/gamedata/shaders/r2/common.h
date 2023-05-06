5824e2cadadb3b8ef173de2e8c30ea9b8b4f3459
xrMorrazzzz <123486080+morrazzzz@users.noreply.github.com>
2023-05-07 00:46:47 +0300

    Фикс AO для r2, r3, r4
    Co-Authored-By: Deathman00 <104826306+Deathman00@users.noreply.github.com>

--- Resources/Gamedata/gamedata/shaders/r2/common.h
+++ Resources/Gamedata/gamedata/shaders/r2/common.h
@@ -284,12 +284,12 @@ half3   p_hemi          (float2 tc)                         {
 
 half   get_hemi( half4 lmh)
 {
-	return lmh.a;
+	return (lmh.x + lmh.y + lmh.z) / 3;
 }
 
 half   get_sun( half4 lmh)
 {
-	return lmh.g;
+	return lmh.a;
 }
 
 //	contrast function
